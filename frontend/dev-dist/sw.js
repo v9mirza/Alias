b7515790de4ec43aa70e742b16046f81beef67b4
v9mirza <v9mirza@gmail.com>
2026-06-19 14:41:34 +0530

    commit

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.q456f2b5rdk"
+    "revision": "0.p9359phlbjc"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
